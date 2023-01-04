7b58b961bf67ae034ee8b8acc1f9766d56a63551
alex-willi <alex.lawson.williams@gmail.com>
2023-01-04 09:07:35 -0600

    author post route works

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ const app = express()
 const cors = require('cors')
 const morgan = require('morgan')
 require('./config/db.connection')
+const authorsController = require('./controllers/author_controller')
 require('dotenv').config()
 const { PORT } = process.env
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(cors())
-
+app.use('/home',authorsController)
 
 app.get('/', (req,res)=>{
-    res.send("Home")
+    res.redirect('/home')
+    res.send("home")
 })
 
 app.listen(PORT, ()=>console.log(`Listening to port: ${PORT}`))
