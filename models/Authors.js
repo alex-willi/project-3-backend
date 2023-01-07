ddc5a666fc8535c01f8df60e6e58164cb7338c99
alex-willi <alex.lawson.williams@gmail.com>
2023-01-07 12:25:43 -0600

    back end

--- models/Authors.js
+++ models/Authors.js
@@ -3,11 +3,8 @@ const mongoose = require('mongoose')
 const authorsSchema = new mongoose.Schema({
     name:{
         type:String
-    },
-    post:[{
-        type: mongoose.Types.ObjectId,
-        ref: "posts"
-    }]
+    }
+  
 })
 
 const Authors = mongoose.model("authors", authorsSchema)
